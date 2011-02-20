0e8fadc5f6cef4b108c75fed197406b27a38552b
Noah Callaway <ncallaway@gmail.com>
2011-02-20 13:04:12 -0800

    Update field to draw diagonal lines

--- Pixl Sport/Field.cs
+++ Pixl Sport/Field.cs
@@ -68,39 +68,63 @@ namespace Pixl_Sport
         private void drawLines(SpriteBatch batch)
         {
             Vector2 padding = sizeMultiplier * PADDING_SIZE;
-            int thinThickness = (int)sizeMultiplier * 2;
             int thickThickness = (int)sizeMultiplier * 4;
 
             Vector2 origin = Position + padding;
 
             int fieldHeight = 432 * (int)sizeMultiplier;
             int endZoneWidth = 50 * (int)sizeMultiplier;
-            int halfField = 300 * (int)sizeMultiplier;
-            int fullField = 600 * (int)sizeMultiplier;
+            int fieldWidth = 700 * (int)sizeMultiplier;
 
             /* Vertical lines */
+            Vector2 thickLineWidthOffset = new Vector2(thickThickness, 0);
+            Vector2 thickLineHeightOffset = new Vector2(0, thickThickness);
+            Vector2 fieldHeightOffset = new Vector2(0, fieldHeight);
+            Vector2 goalHeightOffset = new Vector2(0, 80);
+            Vector2 endzoneWidthOffset = new Vector2(endZoneWidth, 0);
+            Vector2 fieldWidthOffset = new Vector2(fieldWidth, 0);
+            Vector2 openSpaceSixthWidthOffset = (fieldWidthOffset - 2 * endzoneWidthOffset) / 6f;
 
-            Rectangle backLine = new Rectangle((int)origin.X, (int)origin.Y, thickThickness, fieldHeight);
-            Rectangle goalLine = new Rectangle((int)origin.X + endZoneWidth - thickThickness, (int)origin.Y, thickThickness, fieldHeight);
-            Rectangle midLine = new Rectangle((int)origin.X + endZoneWidth + halfField, (int)origin.Y, thinThickness, fieldHeight);
+            Vector2 topLeft = origin;
+            Vector2 bottomLeft = topLeft + fieldHeightOffset;
+            Vector2 centreLeft = topLeft + fieldHeightOffset / 2f;
+            Vector2 topRight = topLeft + fieldWidthOffset;
+            Vector2 bottomRight = bottomLeft + fieldWidthOffset;
+            Vector2 centreRight = topRight + fieldHeightOffset / 2f;
 
-            batch.Draw(pixels, backLine, COLOR_LINES);
-            batch.Draw(pixels, goalLine, COLOR_LINES);
-            batch.Draw(pixels, midLine, COLOR_LINES);
+            Vector2 end = origin;
+            end.Y += fieldHeight;
 
-            goalLine = new Rectangle((int)origin.X + endZoneWidth + fullField + thickThickness, (int)origin.Y, thickThickness, fieldHeight);
-            backLine = new Rectangle((int)origin.X + endZoneWidth + fullField + endZoneWidth, (int)origin.Y, thickThickness, fieldHeight);
+            /* BACK LINES */
+            drawLine(batch, COLOR_LINES, topLeft, bottomLeft, 2, 2);
+            drawLine(batch, COLOR_LINES, topRight, bottomRight, 2, 2);
+            
+            /* GOAL LINES */
+            drawLine(batch, COLOR_LINES, topLeft + endzoneWidthOffset - thickLineWidthOffset, 
+                                        bottomLeft + endzoneWidthOffset - thickLineWidthOffset, 2, 2);
+            drawLine(batch, COLOR_LINES, topRight - endzoneWidthOffset + thickLineWidthOffset,
+                                        bottomRight - endzoneWidthOffset + thickLineWidthOffset, 2, 2);
 
-            batch.Draw(pixels, goalLine, COLOR_LINES);
-            batch.Draw(pixels, backLine, COLOR_LINES);
+            /* CENTER LINE */
+            drawLine(batch, COLOR_LINES, topLeft + fieldWidthOffset / 2f, bottomLeft + fieldWidthOffset / 2f, 2, 1);
 
             /* Horizontal Lines */
+            drawLine(batch, COLOR_LINES, topLeft, topRight, 2, 2);
+            drawLine(batch, COLOR_LINES, bottomLeft - thickLineHeightOffset, bottomRight - thickLineHeightOffset, 2, 2);
+            drawLine(batch, COLOR_LINES, centreLeft - goalHeightOffset / 2f, centreRight - goalHeightOffset / 2f,
+                2, 1, true);
 
-            Rectangle topLine = new Rectangle((int)origin.X, (int)origin.Y, fullField + (2 * endZoneWidth), thickThickness);
-            Rectangle bottomLine = new Rectangle((int)origin.X, (int)origin.Y + fieldHeight - thickThickness, fullField + (2 * endZoneWidth), thickThickness);
+            drawLine(batch, COLOR_LINES, centreLeft + goalHeightOffset / 2f, centreRight + goalHeightOffset / 2f,
+                2, 1, true);
 
-            batch.Draw(pixels, topLine, COLOR_LINES);
-            batch.Draw(pixels, bottomLine, COLOR_LINES);
+
+            /* Diagonal Lines!! */
+            drawLine(batch, COLOR_LINES, topLeft + endzoneWidthOffset + openSpaceSixthWidthOffset,
+               bottomLeft + endzoneWidthOffset + 2 * openSpaceSixthWidthOffset - thickLineHeightOffset, 2, 1);
+
+            drawLine(batch, COLOR_LINES,
+               bottomLeft + endzoneWidthOffset + 4 * openSpaceSixthWidthOffset - thickLineHeightOffset,
+               topLeft + endzoneWidthOffset + 5 * openSpaceSixthWidthOffset, 2, 1);
         }
 
         private void drawEndzone(SpriteBatch batch)
@@ -141,6 +165,87 @@ namespace Pixl_Sport
             position = new Vector2(origin.X + 2 * endZoneWidth + fullField + centered.X - thickThickness + thinThickness, (int)origin.Y + thickThickness + centered.Y);
 
             batch.DrawString(pixelFont, ENDZONE_TEXT_2, position, COLOR_ENDZONE_TEXT, (float)Math.PI / 2f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+        }
+
+
+        private void drawLine(SpriteBatch batch, Color color, Vector2 start, Vector2 end, int pixelThickness, int lineThickness)
+        {
+            drawLine(batch, color, start, end, pixelThickness, lineThickness, false);
+        }
+
+        private void drawLine(SpriteBatch batch, Color color, Vector2 start, Vector2 end, int pixelThickness, int lineThickness, bool dashed)
+        {
+            /* Guarantee start X is less than end X w/ swap */
+            if (end.X < start.X) {
+                Vector2 tmp = start;
+                start = end;
+                end = tmp;
+            }
+
+            int startCellX = (int)start.X;
+            int startCellY = (int)start.Y;
+
+            int endCellX = (int)end.X;
+            int endCellY = (int)end.Y;
+
+            if (endCellX == startCellX) {
+                /* VERTICAL LINE!! */
+                Rectangle dest = new Rectangle(startCellX, Math.Min(endCellY, startCellY), lineThickness * pixelThickness, Math.Abs(endCellY - startCellY));
+                batch.Draw(pixels, dest, color);
+                return;
+            }
+
+
+            int currentCellX = startCellX;
+            int currentCellY = startCellY + pixelThickness * lineThickness;
+
+            int dashCount = 0;
+
+            while (currentCellX <= endCellX) {
+                /* Find intersection with line and X value */
+                float rightX = currentCellX + (lineThickness * pixelThickness);
+
+                float t = (rightX - start.X) / (end.X - start.X);
+                if (t < 0) { t = 0; }
+                if (t > 1) { t = 1; }
+                float intY = ((t * end.Y) + ((1 - t) * start.Y));
+
+                bool goingUp = startCellY < endCellY;
+
+                int steps = 0;
+
+                steps = (int)((currentCellY - intY) / pixelThickness);
+                
+                if (steps >= 0 && steps < lineThickness) {
+                    steps = lineThickness;
+                }
+
+                if (steps < 0 && steps > -lineThickness) {
+                    steps = lineThickness;
+                }
+
+                int currentStopCellY = currentCellY - steps * pixelThickness;
+
+                int yStart = (goingUp) ? currentCellY : currentStopCellY;
+                int height = Math.Abs(currentCellY - currentStopCellY);
+
+                Rectangle dest = new Rectangle(currentCellX, yStart, lineThickness * pixelThickness, height);
+                batch.Draw(pixels, dest, color);
+
+                currentCellX += pixelThickness * lineThickness;
+                dashCount += pixelThickness;
+
+                if (dashed && dashCount > 10) {
+                    dashCount = 0;
+                    currentCellX += 20 * pixelThickness;
+                }
+
+                if (currentStopCellY >= currentCellY) {
+                    currentCellY = currentStopCellY - pixelThickness * lineThickness;
+                } else {
+                    currentCellY = currentStopCellY + pixelThickness * lineThickness;
+                }
+            }
         }
     }
 }
