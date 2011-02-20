011903805d8f1c779173f58a948d8383a0198083
Noah Callaway <ncallaway@gmail.com>
2011-02-20 09:26:10 -0800

    Create Ball

---
+++ Pixl Sport/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pixl_Sport
+{
+    class Ball
+    {
+        private static readonly Color COLOR_BALL = new Color(96, 51, 17);
+
+        private Vector2 position;
+        public Vector2 Position { get { return position; } set { position = value; } }
+
+        public void Draw(SpriteBatch batch, Texture2D pixels, Vector2 fieldOrigin, uint scaleSize)
+        {
+            Vector2 drawLocation = (fieldOrigin + position) * scaleSize;
+            Rectangle destination = new Rectangle((int)drawLocation.X, (int)drawLocation.Y, 2 * (int)scaleSize, 2 * (int)scaleSize);
+
+            batch.Draw(pixels, destination, COLOR_BALL);
+        }
+    }
+}
