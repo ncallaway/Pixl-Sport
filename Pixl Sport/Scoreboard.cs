247fd04b21cdaeee28642302628ccffbb4435c35
Noah Callaway <ncallaway@gmail.com>
2011-02-20 09:56:27 -0800

    Initial SB

---
+++ Pixl Sport/Scoreboard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pixl_Sport
+{
+    class Scoreboard
+    {
+        public String HomeTeam;
+        public String AwayTeam;
+
+        public int HomeScore;
+        public int AwayScore;
+
+        public int Qtr;
+        public String Mode;
+        public TimeSpan timeRemaining;
+    }
+}
